53644ee23ccb5a41f247fc080dca2aa11d08ada5
Kevin Kipper <kevinkippervfx@socal.rr.com>
2023-10-13 15:08:33 -0700

    v2 new gui analyze project first then rename nodes

--- tg_rename_1_1_1_nodes.py
+++ tg_rename_1_1_1_nodes.py
@@ -1,91 +1,151 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
-from tkinter.filedialog import asksaveasfile
-from tkinter.filedialog import askopenfile
 import traceback
 import terragen_rpc as tg
 
 gui = Tk()
-gui.title("tg_rename_1_1_1_nodes.py")
-gui.geometry("320x140")
+gui.geometry("650x640")
+gui.title("wip_rename_1_1_1_nodes.py")
 gui.config(bg="#89B2B9")
 
-gui.rowconfigure(0,weight=2)
+gui.rowconfigure(0,weight=1)
 gui.rowconfigure(1,weight=1)
-gui.columnconfigure(0,weight=1)
-gui.columnconfigure(1,weight=1)
-gui.columnconfigure(2,weight=1)
+gui.rowconfigure(2,weight=1)
+gui.rowconfigure(3,weight=1)
 
-frame0 = LabelFrame(gui,text="Display results in order of ...",relief=FLAT,bg="#B8DBD0",padx=4)
-frame1 = LabelFrame(gui,bg="#89B2B9",relief=FLAT)
-frame0.grid(row=0,column=0,padx=10,pady=10,sticky="WENS")
-frame1.grid(row=1,column=0,padx=4,pady=0)
+frame1 = Frame(gui,padx=10,pady=10,bg="#B8DBD0") # listbox
+frame1.grid(row=0,column=0,padx=4,pady=4,sticky="WENS")
+frame2 = Frame(gui,padx=10,pady=10,bg="#CBAE98") # buttons
+frame2.grid(row=2,column=0,padx=4,pady=4,sticky="WENS")
+frame3 = Frame(gui,padx=10,pady=10,bg="#B8DBD0") # counter label
+frame3.grid(row=1,column=0,padx=4,pady=2,sticky="WENS")
 
-# global variables
-search_pattern = "_1"
-all_names_in_project = []
-new_names = []
-suggested_name = ""
-list_of_tuples = []
-button_choice = IntVar()
-button_choice.set(1)
+def analyze_project():
+    update_progress_status("Analyzing project. This may take a moment.")
+    gui.update()
+    analyze()
+    update_progress_status('Number of nodes in project ending with "_1" is')
 
-def get_nodes_in_project(): # gets the root nodes and passes them to the in_node function
+def analyze():
+    listbox_delete_all()
+    global all_names_in_project, names_to_rename, ids_to_rename, paths_to_rename
     try:
-        file_path = tg.project_filepath()
-        if file_path == "":
-            file_path = "untitled project"
-
         project = tg.root()
-        names, ids = get_nodes_in_node(project)
+        all_names_in_project, names_to_rename, ids_to_rename, paths_to_rename = get_nodes_in_node(project) # may not need globals this way
+    except ConnectionError as e:
+        popup_warning("Terragen RPC connection error",str(e))
+    except TimeoutError as e:
+        popup_warning("Terragen RPC timeout error",str(e))
+    except tg.ReplyError as e:
+        popup_warning("Terragen RPC reply error",str(e))
+    except tg.ApiError:
+        popup_warning("Terragen RPC API error",traceback.format_exc())
         
-        return names, ids, file_path
-    
-    except ConnectionError as e:
-        popup_warning("Terragen RPC connection error",str(e))
-        return [],[],"Terragen RPC connection error"
-    except TimeoutError as e:
-        popup_warning("Terragen RPC timeout error",str(e))
-        return [],[],"Terragen RPC timeout error"
-    except tg.ReplyError as e:
-        popup_warning("Terragen RPC reply error",str(e))
-        return [],[],"Terragen RPC reply error"
-    except tg.ApiError:
-        popup_warning("Terragen RPC API error",traceback.format_exc())
-        return [],[],"Terragen RPC API error"
+    if len(paths_to_rename) == 0:
+        popup_info("tg_rename_1_1_1_ndoes.py", "No nodes found in project ending in '_1'.")    
+    listbox_populate(paths_to_rename)
 
-def get_nodes_in_node(in_node_id):
-    global search_pattern
-    node_names = []
-    node_ids = []    
+def update_progress_status(update_msg):
+    progress_status.set(update_msg)
+
+def get_nodes_in_node(in_node):
+    names_in_project_list = []
+    names_to_rename_list = []
+    paths_to_rename_list = []
+    ids_to_rename_list = []
     try:
-        all_children_ids = in_node_id.children()
-        for child in all_children_ids:
-            child_name = child.name()
-            node_names.append(child_name)
-            if child_name[-2:] == search_pattern:
-                child_name_id = (child_name, child) # child name and node id
-                node_ids.append(child_name_id)
-            deeper_names, deeper_ids = get_nodes_in_node(child)
-            node_names.extend(deeper_names)
-            node_ids.extend(deeper_ids)
-        return node_names,node_ids
-    
+        ids_of_children = in_node.children()
+        if ids_of_children:
+            for child_id in ids_of_children:
+                child_name = child_id.name()
+                child_path = child_id.path()
+                names_in_project_list.append(child_name)
+                if child_name[-2:] == "_1":
+                    names_to_rename_list.append(child_name)
+                    paths_to_rename_list.append(child_path)
+                    ids_to_rename_list.append(child_id)
+                deeper_all_names, deeper_child_names, deeper_child_ids, deeper_child_paths = get_nodes_in_node(child_id)
+                names_in_project_list.extend(deeper_all_names)
+                names_to_rename_list.extend(deeper_child_names)
+                paths_to_rename_list.extend(deeper_child_paths)
+                ids_to_rename_list.extend(deeper_child_ids)
+        num_nodes_to_rename.set(len(names_to_rename_list))
     except ConnectionError as e:
         popup_warning("Terragen RPC connection error",str(e))
-        return [],[]
     except TimeoutError as e:
         popup_warning("Terragen RPC timeout error",str(e))
-        return [],[]
     except tg.ReplyError as e:
         popup_warning("Terragen RPC reply error",str(e))
-        return [],[]
     except tg.ApiError:
         popup_warning("Terragen RPC API error",traceback.format_exc())
-        return [],[]
 
-def names_with_numeric_suffix(truncate_name):
+    return names_in_project_list,names_to_rename_list,ids_to_rename_list,paths_to_rename_list
+
+def listbox_populate(paths_to_rename):
+    for item in paths_to_rename:
+        listbox_of_nodes.insert(END,item)
+
+def listbox_select_all():
+    for i in range(listbox_of_nodes.size()):
+        listbox_of_nodes.selection_set(i)
+
+def listbox_select_none():
+    listbox_of_nodes.selection_clear(0,END)
+
+def listbox_delete_all():
+    listbox_of_nodes.delete(0,END)
+
+def rename_selected_nodes():
+    selected_indices = listbox_of_nodes.curselection() # returns tuple of integers of selection indices
+    global new_names_to_compare, names_to_rename    
+    global paths_to_rename    
+    new_names_to_update = []
+    selected_node_ids_to_rename = []
+    selected_indices_actually_update = [] # need this to update the listbox properly
+    selected_node_new_paths = []
+    if selected_indices:
+        for i in selected_indices:
+            current_node_id = ids_to_rename[i]
+            truncate_name = names_to_rename[i]
+            if truncate_name[-2:] == "_1": # check pattern because update function can rename listbox items and they can be selected again
+                while (truncate_name[-2:] == "_1"):
+                        truncate_name = truncate_name[:-2]
+                if truncate_name[-2:].isdigit():
+                    new_node_name = names_with_numeric_suffix(truncate_name,all_names_in_project,new_names_to_compare)
+                    new_names_to_compare.append(new_node_name)
+                    new_names_to_update.append(new_node_name)
+                    selected_node_ids_to_rename.append(current_node_id)
+                    selected_indices_actually_update.append(i)
+                else:
+                    new_node_name = names_with_alphabetic_suffix(truncate_name,all_names_in_project,new_names_to_compare)
+                    new_names_to_compare.append(new_node_name)
+                    new_names_to_update.append(new_node_name)
+                    selected_node_ids_to_rename.append(current_node_id)
+                    selected_indices_actually_update.append(i)
+
+        selected_node_new_paths = update_project(selected_node_ids_to_rename,new_names_to_update)
+        if len(selected_node_new_paths) == 0:
+            # if nothing returned from update function it means either an rpc exception possibly occured and TG is not running or the nodes were already renamed
+            # if tg not running clear the listbox and return everything to the default state
+            try:
+                x = tg.root()
+            except ConnectionError:
+                listbox_delete_all()
+                return_globals_to_default_state()
+
+    else:
+        popup_info("tg_rename_1_1_1_nodes.py","Please select nodes first.")
+
+    # update the list box to display new node name.  In this way, the indexes are preserved even if the value displayed changes.
+    if selected_indices_actually_update and selected_node_new_paths:
+        for index, item in enumerate(selected_indices_actually_update):
+            listbox_of_nodes.delete(item)
+            listbox_of_nodes.insert(item,selected_node_new_paths[index])
+            names_to_rename[item] = new_names_to_update[index] # updates the original list, otherwise it picks up the old 
+            paths_to_rename[item] = selected_node_new_paths[index]
+
+def names_with_numeric_suffix(truncate_name,all_nodes,new_names_to_compare):
     does_name_already_exist = 1 # assume exists
     starting_number = int(truncate_name[-2:])
     increment_by = 0
@@ -93,127 +153,108 @@ def names_with_numeric_suffix(truncate_name):
         increment_by = increment_by + 1
         next_number = starting_number + increment_by
         suggested_name = truncate_name[:-2] + str(next_number).zfill(2)
-        name_exist = does_name_exist_in_project(suggested_name)
+        name_exist = does_name_exist_in_project(suggested_name,all_nodes,new_names_to_compare)
         if name_exist == 0: # means suggested name is not in use
             does_name_already_exist = -1
     return(suggested_name)
 
-def names_with_alphabetic_suffix(truncate_name):
+def names_with_alphabetic_suffix(truncate_name,all_nodes,new_names_to_compare):
     does_name_already_exist = 1 # assume exists
     increment_by = 0
     while does_name_already_exist >= 0:
         increment_by = increment_by + 1
         suggested_name = truncate_name + " " + str(increment_by).zfill(2)
-        name_exist = does_name_exist_in_project(suggested_name)
+        name_exist = does_name_exist_in_project(suggested_name,all_nodes,new_names_to_compare)
         if name_exist == 0:
             does_name_already_exist = -1
     return (suggested_name)
 
-def does_name_exist_in_project(name): # this function checks to see if the suggested name already exists in the list of all names and list of new names
-    global all_names_in_project
-    global new_names
+def does_name_exist_in_project(name,all_nodes,new_names_to_compare): # this function checks to see if the suggested name already exists in the list of all names and list of new names
     exist = 1 # assume exists
-    for i in all_names_in_project:
+    for i in all_nodes:
         if name  == i:
             return(exist)
-    if len(new_names) > 0: # first time through, won't be anything in the list
-        for i in new_names:
+    if len(new_names_to_compare) > 0: # first time through, won't be anything in the list
+        for i in new_names_to_compare:
             if name == i:
                 return(exist)
     exist = 0 # name doesn't exist yet
     return (exist)
 
-def popup_save_results_to_disk(summary):
-    response = messagebox.askyesno(title="tg_rename_1_1_1_nodes.py",message=summary)
-    if response == True:
-        write_results_to_disk(summary)
-    if response == False:
-        pass
-
-def write_results_to_disk(summary):
-    my_filetypes = [("Text document","*.txt"),("All files","*.*")]
-    file = asksaveasfile(filetypes= my_filetypes, defaultextension=my_filetypes)
-    file.write(summary[:-30]) # don't need to write the ending question to disk, just the data.
-    file.close()
+def popup_info(title_text,message_text):
+    messagebox.showinfo(title=title_text,message=message_text)
 
 def popup_warning(message_type,message_description):
     messagebox.showwarning(title=message_type, message = message_description)
 
-def update_project(new_names): # argument is a list of tuples. elements are new name and node path and old node name
+def update_project(selected_node_ids_to_rename,new_names_to_update): # updates the node's name and returns a list of new auto-generated node paths
+    auto_update_path = []
     try:
-        for item in new_names:
-            try:
-                node_id = item[1]
-                old_name = node_id.name()
-                node_id.set_param('name',item[0])
-            except AttributeError as e:  # this occurred when a node was renamed that also had child nodes in its internal node network. Should no longer occur as using node ids not paths
-                print ("FAILED on ",item," ",str(e)) # not handled well
+        for counter, item in enumerate(selected_node_ids_to_rename):
+            item.set_param('name',new_names_to_update[counter])
+            auto_update_path.append(item.path())
+        return auto_update_path            
     except ConnectionError as e:
         popup_warning("Terragen RPC connection error",str(e))
+        return auto_update_path
     except TimeoutError as e:
         popup_warning("Terragen RPC timeout error",str(e))
+        return auto_update_path
     except tg.ReplyError as e:
         popup_warning("Terragen RPC reply error",str(e))
+        return auto_update_path
     except tg.ApiError:
         popup_warning("Terragen RPC API error",traceback.format_exc())
-
-def summary_of_changes(list_of_tuples,project_file_path):
-    user_choice = button_choice.get()    
-    compare_results_string = project_file_path + "\n\n"
-    sorted_list_of_tuples = list_of_tuples
-
-    if user_choice == 2:
-        sorted_list_of_tuples = sorted(list_of_tuples,key=lambda x: x[2])
-    elif user_choice == 3:
-        sorted_list_of_tuples = sorted(list_of_tuples,key=lambda x: x[2],reverse=True)
+        return auto_update_path
     
-    for item in sorted_list_of_tuples:
-        compare_results_string = compare_results_string + str(item[2]) + "  to  " + str(item[0] + "\n")
+def return_globals_to_default_state():
+    global all_names_in_project, names_to_rename, paths_to_rename, ids_to_rename, new_names_to_compare    
+    all_names_in_project = []
+    names_to_rename = []
+    paths_to_rename = []
+    ids_to_rename = []
+    new_names_to_compare = []
+    num_nodes_to_rename.set(0)
+    update_progress_status('Click the "Analyze project" button to start.  It may take a moment to process the entire project.')    
 
-    compare_results_string = compare_results_string + "\n" + str(len(list_of_tuples)) + " nodes renamed. \n\n"
-    compare_results_string = compare_results_string + "SAVE REPORT SUMMARY TO DISK? \n"
+# global variables
+num_nodes_to_rename = IntVar()
+all_names_in_project = []
+names_to_rename = []
+paths_to_rename = []
+ids_to_rename = []
+new_names_to_compare = []
+progress_status = StringVar()
+update_progress_status('Click the "Analyze project" button to start.  It may take a moment to process the entire project.')
 
-    return compare_results_string
+# gui components
+listbox_of_nodes = Listbox(frame1,width=100,height=30,selectmode=MULTIPLE)
+listbox_of_nodes.grid(row=0,column=0,sticky="WENS")
 
-def main():
-    global all_names_in_project
-    global list_of_tuples
-    all_names_in_project.clear()
-    list_of_tuples.clear()
-    all_names_in_project, nodes_to_replace,project_file_path = get_nodes_in_project() # step 1 - get two lists. Names of all nodes. List of tuples (name, path) (name, node_ids) and file path
-    if len(nodes_to_replace) > 0:
-        for node in nodes_to_replace: # step 2 - remove search pattern from name
-            truncate_name = node[0]
-            while (truncate_name[-len(search_pattern):] == search_pattern):
-                    truncate_name = truncate_name[:-len(search_pattern)]
+listbox_scrollbar = Scrollbar(frame1,orient="vertical")
+listbox_scrollbar.config(command=listbox_of_nodes.yview)
+listbox_scrollbar.grid(row=0,column=1,sticky="NS")
 
-            if truncate_name[-2:].isdigit():
-                    new_node_name = names_with_numeric_suffix(truncate_name)
-                    new_names.append(new_node_name)
-            else:        
-                new_node_name = names_with_alphabetic_suffix(truncate_name)
-                new_names.append(new_node_name)
-            
-            temp_tuple = (new_node_name, node[1], node[0]) # new node name, node id, old node name
-            list_of_tuples.append(temp_tuple)
+listbox_of_nodes.config(yscrollcommand=listbox_scrollbar.set)
 
-        # update project via rpc
-        update_project(list_of_tuples)
-    else:
-        list_of_tuples.clear() # so that when the script is run again without closing the previous contents are purged
+# labels
+progress_label = Label(frame3,textvariable=progress_status,bg="#B8DBD0")
+progress_label.grid(row=1,column=0,columnspan=3,sticky="w")
 
-    # summary report
-    summary = summary_of_changes(list_of_tuples,project_file_path)
-    popup_save_results_to_disk(summary)
+total_nodes_to_rename_label = Label(frame3,textvariable=num_nodes_to_rename,bg="#B8DBD0")
+total_nodes_to_rename_label.grid(row=1,column=3)
 
-r1 = Radiobutton(frame0,text = 'Project order',variable=button_choice, value=1,bg="#B8DBD0")
-r1.grid(row=1,column=0,padx=4,pady=8)
-r2 = Radiobutton(frame0,text = 'A - Z order',variable=button_choice, value=2,bg="#B8DBD0")
-r2.grid(row=1,column=1,padx=4,pady=8)
-r3 = Radiobutton(frame0,text = 'Z - A order',variable=button_choice, value=3,bg="#B8DBD0")
-r3.grid(row=1,column=2,padx=4,pady=8)
+# buttons
+analyze_button = Button(frame2,text="Analyze project / Refresh list",command=analyze_project)
+analyze_button.grid(row=1,column=0,padx=4,pady=4)
 
-go_button = Button(frame1,text="Rename nodes",command=main,bg="#EEEE7A")
-go_button.grid(row=0,column=0,padx=4,pady=8)
+select_all_button = Button(frame2, text="Select all",command=listbox_select_all)
+select_all_button.grid(row=1,column=1,padx=20,pady=4)
+
+select_none_button = Button(frame2,text="Select none",command=listbox_select_none)
+select_none_button.grid(row=1,column=2,padx=5,pady=4)
+
+rename_button = Button(frame2,text="Rename nodes",bg='gold',command=rename_selected_nodes)
+rename_button.grid(row=1,column=4,padx=20,pady=4)
 
 gui.mainloop()
